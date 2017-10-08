490aad0c7347fcb26c5e4b0bc7cd532cf8093a3d
rhythm191 <sac2ndg+develop@gmail.com>
2017-10-08 16:31:19 +0900

    add sass compile function

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,16 @@
-webpack = require('webpack')
+var webpack = require('webpack');
+var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-webpack_config = {
+var webpack_config = [{
+  context: path.join(__dirname, 'src'),
   entry: {
-    content_scripts: './src/content_scripts.js',
-    popup: './src/popup.js'
+    content_scripts: './content_scripts.js',
+    popup: './popup.js',
+    popup_style: './popup.sass'
   },
   output: {
-    path: __dirname + '/build/',
+    path: path.join(__dirname, 'build'),
     filename: '[name].js'
   },
   module: {
@@ -18,17 +22,25 @@ webpack_config = {
         query:{
           presets: ['es2015']
         }
+      },
+      {
+        test: /\.sass$/,
+        loader: ExtractTextPlugin.extract('css-loader!sass-loader')
       }
     ]
   },
   devtool: 'inline-source-map',
-  plugins: []
-};
+  plugins: [
+    new ExtractTextPlugin('[name].css')
+  ]
+}];
 
 
 if (process.env.NODE_ENV === 'production') {
-  webpack_config.devtool = false;
-  webpack_config.plugins.push(new webpack.optimize.UglifyJsPlugin())
+  webpack_config.each(function(config) {
+    config.devtool = false
+    config.plugins.push(new webpack.optimize.UglifyJsPlugin())
+  });
 }
 
 module.exports = webpack_config
